d0737db2c383db0a8ed338cdd9c8624146886254
Milad Rahmat Abadi <milad.rahmat@gmail.com>
2025-02-06 18:08:06 +0200

    Completed ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 15:56:57 by mrahmat-          #+#    #+#             */
+/*   Updated: 2025/02/06 18:04:50 by mrahmat-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+std::string	convertString(std::string s) {
+	for (unsigned long x = 0; x < s.length(); x++) {
+		s[x] = toupper(s[x]);
+	}
+	return s;
+}
+
+int	main(int ac, char **av)	{
+	if (ac == 1) {
+		std::cout << "* LOUD UNBEARABLE FEEDBACK NOISE *";
+	}
+	for (int i = 1; i < ac; i++) {
+		std::cout << convertString(av[i]);
+		}
+	std::cout << "\n";
+	return 0;
+}
