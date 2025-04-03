7aaabb48631fa476d5ab2d6979002b2cc16d243f
Milad Rahmat Abadi <milad.rahmat@gmail.com>
2025-04-03 13:39:12 +0300

    Added more tests

--- CPP02/ex02/sources/main.cpp
+++ CPP02/ex02/sources/main.cpp
@@ -3,28 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrahmat- < mrahmat-@student.hive.fi >      +#+  +:+       +#+        */
+/*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 15:43:25 by mrahmat-          #+#    #+#             */
-/*   Updated: 2025/03/22 17:15:38 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2025/04/03 13:31:02 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main( void ) {
+int main( void )
+{
 	Fixed a;
 	Fixed b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed c(2);
+	Fixed d(10);
 	
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
 	
-	std::cout << b << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "pre incrementing a" << std::endl;
+	++a;
+	std::cout << "a is now " << a << std::endl;
+	std::cout << "post incrementing a" << std::endl;
+	a++;
+	std::cout << "a is now " << a << std::endl;
 	
-	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << "b is " << b << std::endl;
 	
+	std::cout << "Greater value out of a and b is " << Fixed::max( a, b ) << std::endl;
+	std::cout << "Smaller value out of a and b is " << Fixed::min( a, b ) << std::endl;
+	std::cout << "d / c\n" << d / c << std::endl;
+	std::cout << "d * c\n" << d * c << std::endl;
+	std::cout << "d + c\n" << d + c << std::endl;
+	std::cout << "d - c\n" << d - c << std::endl;
+	std::cout << "d > c ";
+	if (d > c)
+		std::cout << "YES" << std::endl;
+	else
+		std::cout << "NO" << std::endl;
+	std::cout << "d < c ";
+	if (d < c)
+		std::cout << "YES" << std::endl;
+	else
+		std::cout << "NO" << std::endl;
+	std::cout << "d >= c ";
+	if (d >= c)
+		std::cout << "YES" << std::endl;
+	else
+		std::cout << "NO" << std::endl;
+	std::cout << "d <= c ";
+	if (d <= c)
+		std::cout << "YES" << std::endl;
+	else
+		std::cout << "NO" << std::endl;
+	std::cout << "d == c ";
+	if (d == c)
+		std::cout << "YES" << std::endl;
+	else
+		std::cout << "NO" << std::endl;
+	std::cout << "d != c ";
+	if (d != c)
+		std::cout << "YES" << std::endl;
+	else
+		std::cout << "NO" << std::endl;
 	return 0;
 }
