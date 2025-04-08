1b05d1f1893876cefd65be7b03d1caece4eda5cf
Milad Rahmat Abadi <milad.rahmat@gmail.com>
2025-04-08 13:02:12 +0300

    Added more tests

--- CPP02/ex01/sources/main.cpp
+++ CPP02/ex01/sources/main.cpp
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrahmat- < mrahmat-@student.hive.fi >      +#+  +:+       +#+        */
+/*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 14:09:45 by mrahmat-          #+#    #+#             */
-/*   Updated: 2025/03/22 14:10:19 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2025/04/08 12:57:49 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main( void ) {
+int main( void )
+{
 	Fixed a;
 	Fixed const b( 10 );
 	Fixed const c( 42.42f );
@@ -29,6 +30,11 @@ int main( void ) {
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+	std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+	std::cout << "d is " << d.toFloat() << " as float" << std::endl;
 	
 	return 0;
-	}
+}
