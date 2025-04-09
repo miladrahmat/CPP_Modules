7263cea594f4e3e8dc0f938dd08d5893eecda3e7
Milad Rahmat Abadi <milad.rahmat@gmail.com>
2025-04-09 15:31:33 +0300

    Added more tests

--- CPP03/ex00/sources/main.cpp
+++ CPP03/ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 21:38:59 by mrahmat-          #+#    #+#             */
-/*   Updated: 2025/04/08 18:44:26 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2025/04/09 15:30:52 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,37 @@
 
 int	main()
 {
-	ClapTrap	clap;
-	ClapTrap	trap("Trap");
-	ClapTrap	a(trap);
-	ClapTrap	b;
+	ClapTrap	basic;
+	ClapTrap	energy("Bob");
+	ClapTrap	dead(energy);
 	
-	a = b;
+	basic = dead;
 
-	clap.attack("Trap");
-	trap.takeDamage(0);
-	trap.attack("Clap");
-	clap.takeDamage(0);
-	clap.attack("Trap");
-	trap.takeDamage(0);
-	clap.beRepaired(1);
-	trap.beRepaired(1);
+	std::cout << "\e[1;36m================================ Basic Test ================================\e[0m" << std::endl;
+	basic.attack("Bob");
+	dead.takeDamage(1);
+	basic.attack("Bob");
+	dead.takeDamage(1);
+	basic.attack("Bob");
+	dead.takeDamage(1);
+	energy.beRepaired(1);
+	basic.beRepaired(1);
+
+	std::cout << "\e[1;36m================================ Energy Test ================================\e[0m" << std::endl;
 	for (int i = 0; i <= 10; i++)
 	{
-		clap.attack("Guy");
-		clap.beRepaired(3);
-		clap.takeDamage(2);
+		energy.attack("Guy");
 	}
+	
+	std::cout << "\e[1;36m================================ Dead Test ================================\e[0m" << std::endl;
+	for (int x = 0; x <= 10; x++)
+	{
+		dead.takeDamage(1);
+	}
+
+	std::cout << "\e[1;36m================================ After death Test ================================\e[0m" << std::endl;
+	dead.attack("Guy");
+	dead.beRepaired(1);
+	dead.takeDamage(1);
 	return (0);
 }
