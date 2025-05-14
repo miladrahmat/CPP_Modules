c55fc6a4cd74d5d982783d3186f7a74e087569df
Milad Rahmat Abadi <milad.rahmat@gmail.com>
2025-05-14 15:21:16 +0300

    Completed ex00

--- CPP06/ex00/sources/ScalarConverter.cpp
+++ CPP06/ex00/sources/ScalarConverter.cpp
@@ -6,21 +6,31 @@
 /*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 12:20:51 by mrahmat-          #+#    #+#             */
-/*   Updated: 2025/05/12 14:41:38 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2025/05/14 15:20:14 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+static void	removeZeros(std::string& str)
+{
+	if (str.find('.') != str.npos)
+		str = str.substr(0, str.find_last_not_of('0') + 1);
+	if (str.length() - 1 == str.find('.'))
+		str += '0';
+}
+
 static std::string	getChar(int i)
 {
-	if (i > std::numeric_limits<unsigned char>::max() || i < std::numeric_limits<unsigned char>::min()) {
+	if (i > std::numeric_limits<char>::max() || i < std::numeric_limits<char>::min()) {
 		return ("impossible");
 	}
 	try {
-		unsigned char c = static_cast<unsigned char>(i);
-		if (std::isprint(c) != 0)
-			return (std::to_string(c));
+		char c = static_cast<char>(i);
+		if (std::isprint(c) != 0) {
+			std::string s1{c};
+			return (s1);
+		}
 		else
 			return ("Non displayable");
 	} catch (std::exception& e) {
@@ -50,6 +60,31 @@ static int	getType(std::string& str)
 		return (DOUBLE);
 }
 
+static void	pseudoLiteral(std::string& str, std::string& c_res, std::string& i_res, std::string& f_res, std::string& d_res)
+{
+	c_res = "impossible";
+	i_res = "impossible";
+	if (str.compare("nanf") == 0 || str.compare("-nanf") == 0 || str.compare("+nanf") == 0|| \
+			str.compare("inff") == 0 || str.compare("-inff") == 0 || str.compare("+inff") == 0) {
+		f_res = str;
+		str.pop_back();
+		d_res = str;	
+	}
+	else {
+		f_res = str + "f";
+		d_res = str;
+	}
+}
+
+static void allImpossible(std::string& c_res, std::string& i_res, std::string& f_res, std::string& d_res)
+{
+	c_res = "impossible";
+	i_res = "impossible";
+	f_res = "impossible";
+	d_res = "impossible";
+	printResult(c_res, i_res, f_res, d_res);
+}
+
 void	ScalarConverter::convert(std::string& str)
 {
 	std::string c_res;
@@ -57,18 +92,25 @@ void	ScalarConverter::convert(std::string& str)
 	std::string f_res;
 	std::string d_res;
 	
+	if (str.compare("nan") == 0 || str.compare("-nan") == 0 || str.compare("+nan") == 0 || \
+			str.compare("nanf") == 0 || str.compare("-nanf") == 0 || str.compare("+nanf") == 0 || \
+			str.compare("inf") == 0 || str.compare("-inf") == 0 || str.compare("+inf") == 0 || \
+			str.compare("inff") == 0 || str.compare("-inff") == 0 || str.compare("+inff") == 0) {
+				pseudoLiteral(str, c_res, i_res, f_res, d_res);
+				printResult(c_res, i_res, f_res, d_res);
+				return ;
+			}
 	switch (getType(str)) {
 		case CHAR:
-			if (std::isprint(str.at(0) != 0))
-				c_res = str.at(0);
+			if (std::isprint(str.at(0)) != 0)
+				c_res = str;
 			else
 				c_res = "Non displayable";
-			if (std::isdigit(str.at(0)) != 0) {
-				i_res = std::to_string(str.at(0) - '0');
-				f_res = std::to_string(str.at(0) - '0') + ".0f";
-				d_res = std::to_string(str.at(0) - '0') + ".0";
+			if (std::isdigit(str.at(0)) == 0) {
+				i_res = std::to_string(str.at(0));
+				f_res = std::to_string(str.at(0)) + ".0";
+				d_res = std::to_string(str.at(0)) + ".0";
 			}
-			printResult(c_res, i_res, f_res, d_res);
 			break ;
 		case INT:
 			try {
@@ -77,10 +119,39 @@ void	ScalarConverter::convert(std::string& str)
 				i_res = std::to_string(i);
 				f_res = std::to_string(static_cast<float>(i));
 				d_res = std::to_string(static_cast<double>(i));
-			} catch (std::exception& e) {
-				std::cerr << e.what() << std::endl;
+			} catch (...) {
+				return (allImpossible(c_res, i_res, f_res, d_res));
 			}
-			printResult(c_res, i_res, f_res, d_res);
 			break ;
+		case FLOAT:
+			try {
+				float f = std::stof(str);
+				int i = static_cast<int>(f);
+				c_res = getChar(i);
+				i_res = std::to_string(i);
+				f_res = std::to_string(f);
+				d_res = std::to_string(static_cast<double>(f));
+			} catch (...) {
+				return (allImpossible(c_res, i_res, f_res, d_res));
+			}
+			break ;
+		case DOUBLE:
+			try {
+				double d = std::stod(str);
+				int i = static_cast<int>(d);
+				c_res = getChar(i);
+				i_res = std::to_string(i);
+				f_res = std::to_string(static_cast<float>(d));
+				d_res = std::to_string(d);
+			} catch (...) {
+				return (allImpossible(c_res, i_res, f_res, d_res));
+			}
+			break ;
+		default:
+			return (allImpossible(c_res, i_res, f_res, d_res));;
 	}
+	removeZeros(f_res);
+	f_res += "f";
+	removeZeros(d_res);
+	printResult(c_res, i_res, f_res, d_res);
 }
--- CPP06/ex00/sources/main.cpp
+++ CPP06/ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 12:28:34 by mrahmat-          #+#    #+#             */
-/*   Updated: 2025/05/12 14:24:24 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2025/05/14 12:10:32 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ int main(int ac, char **av)
 {
 	std::string			str;
 	
-	ac = 0;
+	if (ac == 1) {
+		std::cout << "Scalar converter needs something to convert!" << std::endl;
+		return (1);
+	}
 	str = av[1];
 	ScalarConverter::convert(str);
 	return (0);
