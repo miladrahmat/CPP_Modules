1b5581fda70d7d1a5e2eb625c74ed272bc20b487
Milad Rahmat Abadi <milad.rahmat@gmail.com>
2025-05-23 13:04:04 +0300

    Improved shortestSpan() and longestSpan() to be faster and more efficient

--- CPP08/ex01/sources/Span.cpp
+++ CPP08/ex01/sources/Span.cpp
@@ -6,12 +6,13 @@
 /*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 11:45:07 by mrahmat-          #+#    #+#             */
-/*   Updated: 2025/05/22 16:41:59 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2025/05/23 12:53:32 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 #include <limits>
+#include <algorithm>
 
 Span::Span() : _memory(0), _size(0)
 {
@@ -55,50 +56,32 @@ void	Span::addNumber(int num)
 
 int	Span::shortestSpan()
 {
-	std::vector<int>::iterator	start = _memory.begin();
-	std::vector<int>::iterator	end = _memory.end();
+	std::vector<int>			temp(_memory);
+	std::vector<int>::iterator	start = temp.begin();
 	int							res = std::numeric_limits<int>::max();
 	
 	if (_memory.size() == 0 || _memory.size() == 1)
 		throw NoSpanFoundException();
-	while (end > start) {
-		while (start < end) {
-			if (*end - *start >= 0) {
-				if (*end - *start < res)
-					res = *end - *start;
-			} else {
-				if (*start - *end < res)
-					res = *start - *end;
-			}
-			start++;
-		}
-		start = _memory.begin();
-		end--;
+	std::sort(temp.begin(), temp.end());
+	while (start < temp.end()) {
+		res = std::min(std::abs(*start - *(start + 1)), res);
+		start++;
 	}
 	return (res);
 }
 
 int	Span::longestSpan()
 {
-	std::vector<int>::iterator	start = _memory.begin();
-	std::vector<int>::iterator	end = _memory.end();
+	std::vector<int>			temp(_memory);
+	std::vector<int>::iterator	start = temp.begin();
 	int							res = std::numeric_limits<int>::min();
 	
 	if (_memory.size() == 0 || _memory.size() == 1)
 		throw NoSpanFoundException();
-	while (end > start) {
-		while (start < end) {
-			if (*end - *start >= 0) {
-				if (*end - *start > res)
-					res = *end - *start;
-			} else {
-				if (*start - *end > res)
-					res = *end - *start;
-			}
-			start++;
-		}
-		start = _memory.begin();
-		end--;
+	std::sort(temp.begin(), temp.end());
+	while (start < temp.end()) {
+		res = std::max(std::abs(*start - *temp.end()), res);
+		start++;
 	}
 	return (res);
 }
