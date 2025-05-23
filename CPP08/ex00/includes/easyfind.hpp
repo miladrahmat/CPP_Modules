ffd6d57ceafff50034f8a6c3d181c75e32bed354
Milad Rahmat Abadi <milad.rahmat@gmail.com>
2025-05-23 13:04:52 +0300

    Added a const easyfind and more tests

--- CPP08/ex00/includes/easyfind.hpp
+++ CPP08/ex00/includes/easyfind.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrahmat- < mrahmat-@student.hive.fi >      +#+  +:+       +#+        */
+/*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 10:46:15 by mrahmat-          #+#    #+#             */
-/*   Updated: 2025/05/21 11:29:11 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2025/05/23 12:22:09 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 template <typename T>
 typename T::iterator easyfind(T& container, int to_find)
+{
+	return (std::find(container.begin(), container.end(), to_find));
+}
+
+template <typename T>
+typename T::const_iterator easyfind(const T& container, int to_find)
 {
 	return (std::find(container.begin(), container.end(), to_find));
 }
--- CPP08/ex00/sources/main.cpp
+++ CPP08/ex00/sources/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrahmat- < mrahmat-@student.hive.fi >      +#+  +:+       +#+        */
+/*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 11:21:18 by mrahmat-          #+#    #+#             */
-/*   Updated: 2025/05/21 11:31:12 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2025/05/23 12:22:53 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,43 @@
 
 int main()
 {
-	std::vector<int>			vec({1, 10, 20, 44, 89});
-	std::vector<int>::iterator	found;
-	int							to_find = 11;
-	int							to_find1 = 44;
+	{
+		std::vector<int>			vec({1, 10, 20, 44, 89});
+		std::vector<int>::iterator	found;
+		int							to_find = 11;
+		int							to_find1 = 44;
 
-	try {
-		found = easyfind(vec, to_find);
-		std::cout << *found << std::endl;
-	} catch (std::exception& e) {
-		std::cerr << e.what() << std::endl;
+		try {
+			found = easyfind(vec, to_find);
+			std::cout << *found << std::endl;
+		} catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
+		try {
+			found = easyfind(vec, to_find1);
+			std::cout << *found << std::endl;
+		} catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
 	}
-	try {
-		found = easyfind(vec, to_find1);
-		std::cout << *found << std::endl;
-	} catch (std::exception& e) {
-		std::cerr << e.what() << std::endl;
+	{
+		const std::vector<int>			vec({1, 10, 20, 44, 89});
+		std::vector<int>::const_iterator	found;
+		int							to_find = 11;
+		int							to_find1 = 44;
+
+		try {
+			found = easyfind(vec, to_find);
+			std::cout << *found << std::endl;
+		} catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
+		try {
+			found = easyfind(vec, to_find1);
+			std::cout << *found << std::endl;
+		} catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
 	}
 	return 0;
 }
